e5a6eb3a4906c18edb69a078a74f6ab73f1113e8
Bob Swan <bswan@tenstorrent.com>
2024-12-30 13:49:02 -0600

    Modified the messaging to be more precise

--- lib/install.py
+++ lib/install.py
@@ -10,7 +10,7 @@ def install_tool(vendor, tool, version, src, group, dest_host, dest):
 
     check_dest(dest, dest_host)
     
-    logger.info("Installing %s/%s/%s to %s ..." % (vendor,tool,version,dest_host))
+    logger.info("Copying %s/%s/%s to %s ..." % (vendor,tool,version,dest_host))
 
     command = "%s %s --groupmap=\"*:%s\" --rsync-path=\'%s -p %s && %s\' %s/ %s@%s:%s/" % (rsync, rsync_options, cadtools_group, mkdir, dest, rsync, src, cadtools_user, dest_host, dest)
     run_command(command, lib.my_globals.pretend)
