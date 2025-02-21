40231d658090319165407940bf6cb4be6e8aef0f
Bob Swan <bswan@tenstorrent.com>
2025-02-21 15:17:14 -0600

    Updated code that works as intended for all commands in the allowed_commands list Updated allowed_commands to only be the ones called from within cadinstall + whoami (to confirm setuid is working)

--- src/sudo.c
+++ src/sudo.c
@@ -4,32 +4,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
 #define MAX_COMMAND_LENGTH 4096
-#define ALLOWED_COMMANDS_FILE "../etc/allowed_commands"
+#define MAX_OUTPUT_LENGTH 4096
+#define ALLOWED_COMMANDS_FILE "/proj_risc/user_dev/bswan/tools_src/cadinstall/etc/allowed_commands"
 
 int main(int argc, char *argv[]) {
-    char command[MAX_COMMAND_LENGTH];
-
-    strcpy(command, "");
-    int i;
-    for (i=1; i<argc; i++)
-    {
-        strcat(command, argv[i]);
-        strcat(command, " ");
-    }
-    
-    FILE *allowed_commands_file;
-    char allowed_command[MAX_COMMAND_LENGTH];
-    int allowed = 0;
+    char *command;
+    char *arguments[500];
+    int i = 0;
+    int noOfArgs = 0;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <command>\n", argv[0]);
         exit(1);
     }
-    
+ 
+    command = argv[1];
+
+    for (i=1; i<argc; i++)
+    {
+        arguments[noOfArgs] = argv[i];
+        noOfArgs++;
+    }
+    arguments[noOfArgs] = NULL;
+
+    FILE *allowed_commands_file;
+    char allowed_command[MAX_COMMAND_LENGTH];
+    int allowed = 0;
+   
     // Check if the allowed_commands file exists
     if (access(ALLOWED_COMMANDS_FILE, F_OK) == -1) {
         fprintf(stderr, "Allowed commands file does not exist: %s\n", ALLOWED_COMMANDS_FILE);
@@ -46,7 +52,7 @@ int main(int argc, char *argv[]) {
         allowed_command[strcspn(allowed_command, "\n")] = 0;
 
         // fprintf(stdout, "Checking command against:\n\t%s\n", allowed_command);
-        if (strcmp(allowed_command, argv[1]) == 0) {
+        if (strcmp(allowed_command, command) == 0) {
             allowed = 1;
             break;
         }
@@ -56,12 +62,11 @@ int main(int argc, char *argv[]) {
     fclose(allowed_commands_file);
 
     if (!allowed) {
-        fprintf(stderr, "Command not allowed to be run via sudo operation ...\n");
+        fprintf(stderr, "Command not allowed to be run via this utility: %s\n",command);
         exit(1);
     }
 
-    system(command);
-
+    execvp(command, arguments);
     return 0;
 }
 
@@ -69,5 +74,6 @@ int main(int argc, char *argv[]) {
  gcc -o ../bin/.sudo sudo.c
  The program should be owned by cadtools and have the setuid bit set.
  sudo chown cadtools:vendor_tools ../bin/.sudo
- sudo chmod 4755 ../bin/.sudo
+ sudo chmod 755 ../bin/.sudo
+ sudo chmod u+s ../bin/.sudo
 */
