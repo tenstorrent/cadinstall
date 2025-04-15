a9571dbd9d894a1759d8b34482eab053b1a07d66
Bob Swan <bswan@tenstorrent.com>
2025-04-15 10:24:06 -0500

    Reordered some definitions just to make it easier to edit Removed retired/unused variables
    Cleanup in prepartion for opensourcing

--- cadinstaller/tool_defs.py
+++ cadinstaller/tool_defs.py
@@ -12,31 +12,36 @@ global src
 global sitesList
 global group
 
-user = getpass.getuser()
-host = socket.getfqdn()
-cadtools_user = 'cadtools'
-cadtools_group = 'vendor_tools'
-
-#dest = '/tmp/tools_vendor'
+#########################################
+##                                     ##
+## Your installation-specific settings ##
+##                                     ##
+#########################################
+## This is the basepath where all tool installations will live
 dest = '/tools_vendor'
-dest_group = 'cadtools'
+## This is the account that will own all tool installations
+cadtools_user = 'cadtools'
+## This is the default unix group that will protect all tool installations
+cadtools_group = 'vendor_tools'
+## This is the default unix permission set that will protect all tool installations
 dest_mode = 2755
 
+## Required system command paths
 rsync = '/usr/bin/rsync'
 mkdir = '/usr/bin/mkdir'
 curl = '/usr/bin/curl'
-rsync_exclude_file = os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + '/../etc/rsync_exclude_list.txt')
-rsync_options = "-av --chmod=u+rwx,g+rx,o=rx --exclude-from=%s" % (rsync_exclude_file)
 
-# Set up the global variables for the jenkins job
-curl_cmd = curl + ' -X POST -L'
-jenkins_user = "bswan:11ce74b6c978b1484607c6c9168e085b44"
-jenkins_url = 'http://aus-rv-l-7:8081'
-
-## define a hash for machines per site
+## Each site where tools are to be installed at
 siteHash = {
     'aus': 'rv-misc-01.aus2.tenstorrent.com',
     'yyz': 'soc-l-01.yyz2.tenstorrent.com'
 }
+
+## Generic global variables, probably don't need to change any of these
+user = getpass.getuser()
+host = socket.getfqdn()
+
+rsync_exclude_file = os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + '/../etc/rsync_exclude_list.txt')
+rsync_options = "-av --chmod=u+rwx,g+rx,o=rx --exclude-from=%s" % (rsync_exclude_file)
 
 
